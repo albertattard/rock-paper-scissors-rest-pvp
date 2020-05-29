cc3e3adc2b5d7a87becb8e91b8c1dcdd7b3ed4f7
Albert Attard <albert.attard@thoughtworks.com>
2020-05-29 18:43:04 +0200

    Add list open games test

--- src/main/java/demo/games/pvp/PvpGameController.java
+++ src/main/java/demo/games/pvp/PvpGameController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
 public class PvpGameController {
 
@@ -18,5 +20,10 @@ public class PvpGameController {
   @GetMapping( "/game/new/{player1}" )
   public @ResponseBody GameResponse create( final @PathVariable( "player1" ) Hand player1 ) {
     return service.create( player1 );
+  }
+
+  @GetMapping( "/game/list/" )
+  public @ResponseBody List<GameResponse> list() {
+    return service.listOpenGames();
   }
 }
--- src/test/java/demo/games/pvp/PvpGameControllerTest.java
+++ src/test/java/demo/games/pvp/PvpGameControllerTest.java
@@ -1,14 +1,21 @@
 package demo.games.pvp;
 
 import demo.games.shared.Hand;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
-import static org.hamcrest.core.Is.is;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -32,14 +39,51 @@ public class PvpGameControllerTest {
   public void shouldCreateGameAndReturnCode() throws Exception {
 
     final Hand player1 = Hand.ROCK;
-    final String code = "abcdefgh";
+    final GameResponse game = createRandomGame();
 
-    when( service.create( player1 ) ).thenReturn( new GameResponse( code ) );
+    when( service.create( player1 ) ).thenReturn( game );
 
     mockMvc.perform( get( String.format( "/game/new/%s", player1.name() ) ) )
       .andExpect( status().isOk() )
-      .andExpect( jsonPath( "$.code", is( code ) ) );
+      .andExpect( jsonPath( "$.code", is( game.getCode() ) ) );
 
     verify( service, times( 1 ) ).create( player1 );
+  }
+
+  @Test
+  @DisplayName( "should return the list of open games" )
+  public void shouldReturnOpenGames() throws Exception {
+
+    final List<GameResponse> games = createRandomGames( 5 );
+
+    when( service.listOpenGames() ).thenReturn( games );
+
+    final ResultActions result = mockMvc.perform( get( "/game/list/" ) )
+      .andExpect( status().isOk() )
+      .andExpect( jsonPath( "$" ).isArray() )
+      .andExpect( jsonPath( "$", hasSize( games.size() ) ) )
+      .andExpect( jsonPath( "$[*].code", containsInAnyOrder( toGameCode( games ) ) ) );
+
+    verify( service, times( 1 ) ).listOpenGames();
+  }
+
+  private String[] toGameCode( List<GameResponse> games ) {
+    final String[] codes = new String[games.size()];
+    for ( int i = 0; i < games.size(); i++ ) {
+      codes[i] = games.get( i ).getCode();
+    }
+    return codes;
+  }
+
+  private List<GameResponse> createRandomGames( int number ) {
+    final List<GameResponse> games = new ArrayList<>( number );
+    for ( int i = 0; i < number; i++ ) {
+      games.add( createRandomGame() );
+    }
+    return games;
+  }
+
+  private GameResponse createRandomGame() {
+    return new GameResponse( RandomStringUtils.randomAlphanumeric( 8 ) );
   }
 }
